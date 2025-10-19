04b4e90e866af0375f0d601bed39c603fed9e22a
arun <tirumalaarunkumar8@gmail.com>
2025-10-19 20:35:57 +0530

    frontend initialisation(ejs)

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 
 const express=require("express");
+const path=require("path");
 const app=express();
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 const port=3000;
 app.listen(port,()=>{
     console.log(`listening on ${port}`);
 })
 app.get("/",(req,res)=>{
     res.send("<h1>hello world</h1>")
+})
+app.get("/Blogify",(req,res)=>{
+    res.render("home");
 })
