629f2381e9f654a4b62fa12406302393ce95ca6e
arun <tirumalaarunkumar8@gmail.com>
2025-10-20 12:55:40 +0530

    sign up set up

--- index.js
+++ index.js
@@ -5,15 +5,26 @@ const app=express();
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 const port=3000;
+let users=[{username:"arun",
+    password:"Arun@123",
+    firstName:"Arun",
+    lastName:"kumar",
+    email:"arun@123",
+    password:"arun123"
+}]
 app.listen(port,()=>{
     console.log(`listening on ${port}`);
 })
-app.get("/Blogify",(req,res)=>{
+app.get("/blogify",(req,res)=>{
     res.render("home");
 })
-app.get("/Blogify/login",(req,res)=>{
+app.get("/blogify/login",(req,res)=>{
     res.render("login");
 })
-app.get("/Blogify/signup",(req,res)=>{
+app.get("/blogify/signup",(req,res)=>{
     res.render("signup");
+})
+app.get("/blogify/signuppage",(req,res)=>{
+     let {username,password,lastName,firstName,email}=req.query;
+     res.render("home");
 })
